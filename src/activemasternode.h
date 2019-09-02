d07feb27162ae232adcf8ea32d76812dbda2859a
mccrudd3n <49352991+mccrudd3n@users.noreply.github.com>
2019-09-02 23:53:03 +0100

    Copyright

--- src/activemasternode.h
+++ src/activemasternode.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2012 The Bitcoin developers
-// Copyright (c) 2014-2015 The Dash developers
+// Copyright (c) 2014-2020 The Dash developers
 // Copyright (c) 2015-2019 The PIVX developers
 // Copyright (c) 2017-2020 The XUEZ developers
 // Distributed under the MIT software license, see the accompanying
